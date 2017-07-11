f942c6f4460ba046d73163a4f0e1a953499f26dc
remy lichani <rem.lichani@gmail.com>
2017-07-11 11:21:55 +0200

    add index method

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,24 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @tasks = Task.find(params[:id])          # GET /tasks/:id
+  end
+
+  def new           # GET /tasks/new
+  end
+
+  def create        # POST /tasks
+  end
+
+  def edit          # GET /tasks/:id/edit
+  end
+
+  def update        # PATCH /tasks/:id
+  end
+
+  def destroy       # DELETE /tasks/:id
+  end
+end

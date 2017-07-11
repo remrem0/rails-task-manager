c179beca544e71779ffaf66cbe31df6981d22fdd
remy lichani <rem.lichani@gmail.com>
2017-07-11 12:30:57 +0200

    added delete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])          # GET /tasks/:id
+    #set_task
   end
 
   def new           # GET /tasks/new
@@ -14,19 +16,31 @@ class TasksController < ApplicationController
   def create        # POST /tasks
     @task = Task.create(task_params)
 
-    redirect_to tasks_path(@task)
+    redirect_to task_path(@task)
   end
 
   def edit          # GET /tasks/:id/edit
+    #set_task
   end
 
   def update        # PATCH /tasks/:id
+    #set_task
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
   end
 
   def destroy       # DELETE /tasks/:id
+    @task.destroy
+
+    redirect_to tasks_path
   end
 
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :description)
